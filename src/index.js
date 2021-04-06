f09d9792d3c97ed416549210678143448d70a9c4
JAFB321 <jafb321@gmail.com>
2021-04-06 13:16:56 -0700

    refactor: servers create and init, feat: emit sensor updates to web sockets

--- src/index.js
+++ src/index.js
@@ -1,34 +1,33 @@
 const http = require('http');
-const SocketIO = require('socket.io');
 const express = require('express');
+const SocketIO = require('socket.io');
+const sensorsTCP = require('./sensorsTCP');
 
 const app = new express();
+const tcpServer = new sensorsTCP({ HOST: 'localhost', PORT: 4000 });
 const httpServer = http.createServer(app);
-const io = SocketIO().listen(httpServer);
+const webSockets = SocketIO().listen(httpServer);
 
-// const app = require('express')();
-// const http = require('http').Server(app);
-// const io = require('socket.io')(http);
-
-io.on('connection', function (socket) {
+// Websockets
+webSockets.on('connection', function (socket) {
 	console.log('A user connected');
 
-	//Whenever someone disconnects this piece of code executed
 	socket.on('disconnect', function () {
 		console.log('A user disconnected');
 	});
-
-	socket.emit('message', 'TEST');
 });
 
+// Sensors tcp listen
+tcpServer.onSensorValue((sensorInfo) => {
+	console.log(sensorInfo);
+
+	const { value, sensorID } = sensorInfo;
+	webSockets.emit('message', `${sensorID}: ${value}`);
+});
+
+// Serve public assets
 app.use(express.static(__dirname + '/public'));
 
+// Init TCP and http server
 httpServer.listen(3000, () => console.log('Http server running on port 3000'));
-
-const { sensorsTCP } = require('./sensorsTCP');
-
-const tcp = new sensorsTCP({ HOST: 'localhost', PORT: 4000 });
-
-tcp.init(({ sensorID, value }) => {
-	console.log(`${sensorID}: ${value} \n`);
-});
+tcpServer.init();
