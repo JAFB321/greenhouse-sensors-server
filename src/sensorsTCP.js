63a04501f165c2d2c82951cea5ca62e3b862118f
JAFB321 <jafb321@gmail.com>
2021-04-06 13:15:06 -0700

    feat: function to listen sensor updates and save connected sockets

--- src/sensorsTCP.js
+++ src/sensorsTCP.js
@@ -1,32 +1,26 @@
 const net = require('net');
 
 // TCP/IP Server to communicate with the Gateway
-
 class sensorsTCP {
 	constructor({ PORT, HOST }) {
 		this.PORT = PORT;
 		this.HOST = HOST;
 		this.serverTCP = net.createServer();
+		this.sockets = [];
 	}
 
-	init(onSensorValue = ({ sensorID, value }) => {}) {
+	init() {
 		this.serverTCP.on('connection', (socket) => {
+			// Add to connected sockets
+			this.sockets.push(socket);
+
 			console.log(`Client connection from: ${socket.remoteAddress}`);
 			socket.setEncoding('utf8');
-
-			socket.on('data', (data) => {
-				// Gateway sensor info recieved
-				try {
-					const sensor = JSON.parse(data);
-					if (sensor.sensorID && sensor.value) {
-						onSensorValue(sensor);
-					}
-				} catch (error) {}
-			});
 
 			socket.on('close', () => {
 				// Gateway connection closed
 				console.log(`Client connection closed: ${socket.remoteAddress}`);
+				this.sockets = this.sockets.filter((item) => item !== socket);
 			});
 
 			socket.on('error', () => {
@@ -39,6 +33,32 @@ class sensorsTCP {
 			console.log('TCP/IP server running on port 4000')
 		);
 	}
+
+	onSensorValue(listener = ({ sensorID, value }) => {}) {
+		this.sockets.forEach((socket) => {
+			socket.on('data', (data) => {
+				// Gateway sensor info recieved
+				try {
+					const sensor = JSON.parse(data);
+					if (sensor.sensorID && sensor.value) {
+						listener(sensor);
+					}
+				} catch (error) {}
+			});
+		});
+
+		this.serverTCP.on('connection', (socket) => {
+			socket.on('data', (data) => {
+				// Gateway sensor info recieved
+				try {
+					const sensor = JSON.parse(data);
+					if (sensor.sensorID && sensor.value) {
+						listener(sensor);
+					}
+				} catch (error) {}
+			});
+		});
+	}
 }
 
-module.exports = { sensorsTCP };
+module.exports = sensorsTCP;
