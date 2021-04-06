421070a55b036e5abce8827c185a7d748c92bc6d
JAFB321 <jafb321@gmail.com>
2021-04-05 19:10:25 -0700

    feat: Express and websockets server

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-// const http = require('http');
-// const SocketIO = require('socket.io');
-// const express = require('express');
+const http = require('http');
+const SocketIO = require('socket.io');
+const express = require('express');
 
-// const app = new express();
-// const httpServer = http.createServer(app);
-// const io = SocketIO().listen(httpServer);
+const app = new express();
+const httpServer = http.createServer(app);
+const io = SocketIO().listen(httpServer);
 
-// app.use(express.static(__dirname + '/public'));
+// const app = require('express')();
+// const http = require('http').Server(app);
+// const io = require('socket.io')(http);
 
-// httpServer.listen(3000, () => console.log('Http server running on port 3000'));
+io.on('connection', function (socket) {
+	console.log('A user connected');
+
+	//Whenever someone disconnects this piece of code executed
+	socket.on('disconnect', function () {
+		console.log('A user disconnected');
+	});
+
+	socket.emit('message', 'TEST');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+httpServer.listen(3000, () => console.log('Http server running on port 3000'));
 
 const { sensorsTCP } = require('./sensorsTCP');
 
