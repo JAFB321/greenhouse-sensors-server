f015ce0ea36abbd1e1cccf50f34d8fcb9d402995
JAFB321 <jafb321@gmail.com>
2021-04-06 16:44:28 -0700

    feat: show sensor data

--- src/public/index.js
+++ src/public/index.js
@@ -1,5 +1,21 @@
 var socket = io.connect('http://localhost:3000', { forceNew: true });
 
 socket.on('sensor', function (data) {
-	console.log(data);
+	// data:
+	//	[
+	// 		{ id: 'EXAMPLE', values: { value: 312, date: 'date' } },
+	//  	{ id: 'EXAMPLE2', values: { value: 324, date: 'date' } }
+	// ];
+	// (check src/database.js)
+
+	var sensors = JSON.parse(data);
+	var sensorList = document.getElementById('sensors');
+
+	console.log(sensors);
+
+	var info = '';
+	sensors.forEach(({ id, value }) => {
+		info += `<p>${id}: ${value.value}</p>`;
+	});
+	sensorList.innerHTML = info;
 });
