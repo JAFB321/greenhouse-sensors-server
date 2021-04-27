45c4decdf2a8a6cb105b7ec795af51cc3dddf666
JAFB321 <jafb321@gmail.com>
2021-04-26 18:29:10 -0700

    -feat: use sass loaders

--- webpack.config.js
+++ webpack.config.js
@@ -4,4 +4,24 @@ module.exports = {
 		path: __dirname + '/src/public',
 		filename: 'bundle.js',
 	},
+	module: {
+		rules: [
+			{
+				use: 'babel-loader',
+				test: /\.js$/,
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					// Creates `style` nodes from JS strings
+					'style-loader',
+					// // Translates CSS into CommonJS
+					'css-loader',
+					// Compiles Sass to CSS
+					'sass-loader',
+				],
+			},
+		],
+	},
 };
