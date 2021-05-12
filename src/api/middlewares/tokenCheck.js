eebb0e966764fda794682923116efb6234dad8b3
JAFB321 <jafb321@gmail.com>
2021-05-12 15:33:59 -0700

    feat: api middlewares

---
+++ src/api/middlewares/tokenCheck.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+const tokenCheck = (req, res, next) => {
+	try {
+		const token = req.header('token');
+		const secret = req.app.get('JWT_SECRET_KEY');
+		if (token) {
+			if (jwt.verify(token, secret)) {
+				next();
+				return;
+			}
+		}
+
+		res.status(401).json({ error: true, statusCode: 401 });
+	} catch (error) {
+		return res.status(500).json({ error: true, statusCode: 500 });
+	}
+};
+
+module.exports = tokenCheck;
