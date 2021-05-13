5284ca6c3079f15f789baf1816bf8a2b8710ac20
JAFB321 <jafb321@gmail.com>
2021-05-12 18:20:56 -0700

    fix: use database, http server, tcp server and run

--- src/index.js
+++ src/index.js
@@ -1,41 +1,56 @@
+require('dotenv').config();
 const http = require('http');
 const express = require('express');
 const SocketIO = require('socket.io');
+const cors = require('cors');
 const sensorsTCP = require('./gateway/sensorsTCP');
-const { getSensorsLastValues, registerSensorValue } = require('./database/database');
+const ConnectionDB = require('./database/Connection');
+const apiRoutes = require('./api/routes/index');
+const SocketsController = require('./sockets/socketsController');
 
-const app = new express();
-const tcpServer = new sensorsTCP({ HOST: 'localhost', PORT: 4000 });
+// Mongo Database
+const { DB_URL, DB_NAME, DB_USER, DB_PASS } = process.env;
+
+new ConnectionDB({
+	URL: DB_URL || 'mongodb+srv://@cluster0.pwji2.mongodb.net',
+	dbName: DB_NAME,
+	user: DB_USER,
+	pass: DB_PASS,
+}).Connect();
+
+// ------ HTTP Server ------------------------
+const app = express();
 const httpServer = http.createServer(app);
-const webSockets = SocketIO().listen(httpServer);
 
-// Websockets
-webSockets.on('connection', function (socket) {
-	console.log('A user connected');
-
-	socket.on('disconnect', function () {
-		console.log('A user disconnected');
-	});
-
-	socket.on('error', (error) => {
-		console.log(error);
-	});
-});
-
-// Auto send sensor info to clients
-setInterval(() => {
-	const lastSensors = getSensorsLastValues();
-	webSockets.emit('sensor', JSON.stringify(lastSensors));
-}, 1000);
-
-// Sensors tcp listen
-tcpServer.onSensorValue((sensorInfo) => {
-	registerSensorValue(sensorInfo);
-});
-
+// JWT Token secret
+app.set('JWT_SECRET_KEY', process.env.JWT_SECRET_KEY || 'TEAM4');
+// Middlewares
+app.use(express.json());
+app.use(cors());
+// Api Routes
+app.use('/api', apiRoutes);
 // Serve public assets
 app.use(express.static(__dirname + '/public'));
 
+// ------ Websockets ------------------------
+const webSockets = SocketIO(httpServer, {
+	cors: {
+		origin: '*',
+	},
+});
+
+const socketsController = new SocketsController(webSockets);
+
+// ------ TCP Server ------------------------
+const tcpServer = new sensorsTCP({ PORT: process.env.TCP_PORT || 4000 });
+const gatewayController = require('./gateway/gatewayController');
+
+// Sensors tcp listen
+tcpServer.onSensorValue((sensorInfo) => {
+	gatewayController.registerSensorRead(sensorInfo);
+});
+
 // Init TCP and http server
-httpServer.listen(3000, () => console.log('Http server running on port 3000'));
+const port = process.env.PORT || 8080;
+httpServer.listen(port, () => console.log(`Http server running on port ${port}`));
 tcpServer.init();
